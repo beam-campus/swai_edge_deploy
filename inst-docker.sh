e4e245b919dc4d8d5d1dda2d816a28def7f46927
Rafael Lefever <rafael.lefever@discomco.pl>
2024-06-28 12:21:24 +0200

    no nordvpn

--- inst-docker.sh
+++ inst-docker.sh
@@ -26,4 +26,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
