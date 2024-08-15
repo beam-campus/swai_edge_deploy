35407575443b8ef6f57a6f5cbbe8ec267dff7429
Rafael Lefever <rafael.lefever@discomco.pl>
2024-08-15 11:41:51 +0200

    added more verbose play

--- start-play.sh
+++ start-play.sh
@@ -13,9 +13,19 @@ while true; do
   # Select a random container ID
   random_container=$(echo "${containers[@]}" | tr ' ' '\n' | shuf -n 1)
 
+  # Get the name of the selected container
+  container_name=$(docker inspect --format '{{.Name}}' "$random_container" | sed 's/^\/\|\/$//g')
+
+  # Echo the container name before restarting
+  echo "Restarting container: $container_name ($random_container)"
+
+  sleep 5
+
   # Restart the selected container
   docker restart "$random_container"
-  echo "Restarted container: $random_container"
+
+  # Echo the container name after restarting
+  echo "Restarted container: $container_name ($random_container)"
 
   # Sleep for a random time interval between 0 and 120 seconds
   sleep_time=$(shuf -i 0-120 -n 1)
